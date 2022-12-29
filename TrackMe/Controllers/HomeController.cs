653ac70f5530f3216305e815a14e1087a28dc01f
LukeCousins00 <107442903+LukeCousins00@users.noreply.github.com>
2022-12-29 01:39:57 +0000

    initial commit

---
+++ TrackMe/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TrackMe.Models;
+
+namespace TrackMe.Controllers;
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
